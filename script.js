0d206e40a35f039282ef1c752426c89e3d8f7120
Vipul Trimbake <130178066+VipulTrimbake610@users.noreply.github.com>
2023-06-27 07:42:44 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const text = document.getElementById("text");
+const delay = document.getElementById("delay");
+const btn = document.getElementById("btn");
+const output = document.getElementById("output");
+
+function show(){
+	setTimeout(function(){
+		output.innerText = text.value;
+	},delay.value)
+	
+}
+btn.addEventListener("click",function(e){
+	show();
+});
